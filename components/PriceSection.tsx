cfcbe54eea223a512bbc085ae4c9058bf4ee788c
Ibe Noriaki <ibenoriaki@gmail.com>
2024-08-16 03:07:39 +0900

    Update PriceSection.tsx

--- components/PriceSection.tsx
+++ components/PriceSection.tsx
@@ -29,7 +29,7 @@ const PriceSection: React.FC<PriceSectionProps> = ({ id }) => {
         </div>
         <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           <div className="p-8 sm:p-10 lg:flex-auto">
-            <h3 className="text-2xl font-bold tracking-tight text-gray-900">{text.priceSection.planName}</h3>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">{text.priceSection.title}</h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
               {text.priceSection.planDescription}
             </p>
